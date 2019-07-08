f32226adba42058347674855abd3d7f349e7910a
Tyrone Cartwright <tyrone.cartwright@yahoo.com>
2019-07-08 17:09:57 -0400

    MVP met for Day 1

--- basic_hashtable/b_hashtables.py
+++ basic_hashtable/b_hashtables.py
@@ -15,7 +15,9 @@ class Pair:
 # '''
 class BasicHashTable:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity  # Maximum size
+        self.count = 0  # Current size being used
+        self.storage = [None] * capacity
 
 
 # '''
@@ -23,7 +25,12 @@ class BasicHashTable:
 # Research and implement the djb2 hash function
 # '''
 def hash(string, max):
-    pass
+    hash = 3313  # arbitrary number to initialize
+
+    for i in string:
+        hash = ((hash << 5) + hash) + ord(i)
+
+    return hash % max
 
 
 # '''
@@ -32,25 +39,50 @@ def hash(string, max):
 # If you are overwriting a value with a different key, print a warning.
 # '''
 def hash_table_insert(hash_table, key, value):
-    pass
+    new_hash = hash(key, hash_table.capacity)
 
+    if hash_table.storage[new_hash] and hash_table.storage[new_hash][0] != key:
+        print(f"WARNING: OVERWRITING VALUE!!")
+
+    hash_table.storage[new_hash] = (key, value)
+
+    return
 
 # '''
 # Fill this in.
 
 # If you try to remove a value that isn't there, print a warning.
 # '''
-def hash_table_remove(hash_table, key):
-    pass
 
+
+def hash_table_remove(hash_table, key):
+    new_hash = hash(key, hash_table.capacity)
+
+    if not hash_table.storage[new_hash]:
+        return print(f"Key {key} not found.")
+
+    hash_table.storage[new_hash] = None
+
+    return
 
 # '''
 # Fill this in.
 
 # Should return None if the key is not found.
 # '''
+
+
 def hash_table_retrieve(hash_table, key):
-    pass
+    new_hash = hash(key, hash_table.capacity)
+
+    retr = hash_table.storage[new_hash]
+
+    if retr:
+        return retr[1]
+    else:
+        return retr
+
+    return
 
 
 def Testing():
